dd68b38195c375bbc0c616bd762c5b638bed4d4e
joostderuijter <joostderuijter@protonmail.com>
2020-05-03 00:33:18 +0200

    Refactoring + added web page

---
+++ crawler/api.py
@@ -0,0 +1,14 @@
+import flask
+from crawler import order
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/order', methods=['POST'])
+def home():
+    try: 
+        return order()
+    except: 
+        return home()
+
+app.run()
